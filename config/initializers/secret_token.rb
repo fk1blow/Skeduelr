dc0b779949591fe27e3df70b445e106801ebe15c
Dragos Tudorache <hedphug@gmail.com>
2013-10-06 13:16:42 +0300

    Initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skeduelr::Application.config.secret_key_base = '77c44247311e545f0f78360a053f90c8c81bd72139c5eebd82d54331c9f2387ffea2ccbd774bb8dd8ce9770e3fd28b3a9ebc6a67197725c48534a690046e21b5'
